21b446720de59ba178239bbb7bfbe8274dc119d3
seula <seulaayoon@gmail.com>
2023-05-17 14:09:20 -0400

    [ADD] about me html & css

--- seula/src/App.jsx
+++ seula/src/App.jsx
@@ -12,7 +12,14 @@ function App() {
           <span>seulayoon</span>
           <IconButton/>
         </div>
-        <div className="about-me"></div>
+        <div className="about-me">
+          <span>Hello World!</span>
+          <div>
+            <span>I&apos;m </span>
+            <span className='underline'>Seula Yoon.</span>
+          </div>
+          <p className='paragraph'>Based in the Canda, I&apos;m software engineer</p>
+        </div>
         <div className="projects"></div>
       </div>
     </>
