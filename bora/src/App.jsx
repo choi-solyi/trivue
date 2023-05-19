858f6e7e44517233b236c907fa5be923703957a9
양보라 <br.yang@piolink.com>
2023-05-19 16:38:53 +0900

    Style : 라우트 설정

--- bora/src/App.jsx
+++ bora/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App =()=> {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route exact path='/' Component={Home}/>
+        <Route exact path='/hello' Component={HelloWorld}/>
+      </Routes>
+    </Router>
   )
 }
+const Home = () => {
+  return <h1>Home Page</h1>;
+}
 
+const HelloWorld = () => {
+  return <h1>HelloWorld</h1>;
+}
 export default App
