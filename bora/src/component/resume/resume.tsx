5266d7d579fbfebad4ba6510d48e3d5880099062
양보라 <br.yang@piolink.com>
2023-05-24 14:11:53 +0900

    FIX : 폴더 정리

--- bora/src/component/resume/resume.tsx
+++ bora/src/component/resume/resume.tsx
@@ -3,7 +3,7 @@ import "../../assets/css/skill.css";
 import { Info } from "./info";
 import { Skill } from "./skill";
 import { Career } from "./career";
-import photoPath from "../../assets/photo.jpg";
+import photoPath from "../../assets/image/photo.jpg";
 
 export const Resume = (): React.ReactElement => {
   return (
